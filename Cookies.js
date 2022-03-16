ffe82e8b740113e9b98037780c96308dcae7ec24
harunrzl <github@harun.id>
2022-03-16 18:16:48 +0700

    feat: init

---
+++ Cookies.js
@@ -0,0 +1,28 @@
+function getItem(sKey) {
+  if (!sKey) {
+    return null;
+  }
+
+  if (!canUseDOM || !document || !document.cookie) {
+    return false;
+  }
+
+  const regexKeyPart = encodeURIComponent(sKey).replace(/[-.+*]/g, '\\$&');
+  const fullRegex = new RegExp(`(?:(?:^|.*;)\\s*${regexKeyPart}\\s*\\=\\s*([^;]*).*$)|^.*$`);
+
+  return decodeURIComponent(document.cookie.replace(fullRegex, '$1')) || null;
+};
+
+function hasItem(sKey) {
+  if (!sKey) {
+    return false;
+  }
+
+  if (!canUseDOM || !document || !document.cookie) {
+    return false;
+  }
+
+  const regex = new RegExp(`(?:^|;\\s*)${encodeURIComponent(sKey).replace(/[-.+*]/g, '\\$&')}\\s*\\=`);
+
+  return regex.test(document.cookie);
+};
